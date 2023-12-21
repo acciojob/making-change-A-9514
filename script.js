16488b3d15152688ccbd530bc2bc0fcf52f27e47
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-21 20:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const makeChange = (c) => {
-  // your name here
+var makeChange = function(total){
+  var count = 0;
+  var coins = [1, 2, 5, 10, 20, 50, 100, 200];
+
+  var changer = function(index, value){
+
+    var currentCoin = coins[index];
+
+    if( index === 0){
+      if( value % currentCoin === 0){
+        count++;
+      }
+      return;
+    }
+
+    while( value >= 0 ){
+      changer(index-1, value);
+      value -= currentCoin;
+    }
+  }
+  changer(coins.length-1, total);
+  return count;
 };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+makeChange(200);
